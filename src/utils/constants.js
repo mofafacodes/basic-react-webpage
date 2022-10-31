6d966232691e1db8b540635cf82b129f0787cc24
Nutifafa Kpodo <nutifafafagan@gmail.com>
2022-10-31 02:01:23 +0000

    Page data added.

---
+++ src/utils/constants.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 0,
+    title: 'Twitter Link',
+    link: 'https://twitter.com/mofafacodes',
+    btn_id: 'twitter'
+  },
+  {
+    id: 1,
+    title: 'Zuri Team',
+    link: 'https://training.zuri.team/',
+    btn_id: 'books'
+  },
+  {
+    id: 2,
+    title: 'Zuri Books',
+    link: ' http://books.zuri.team',
+    btn_id: 'book__python'
+  },
+  {
+    id: 3,
+    title: 'Python Team',
+    link: 'https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>',
+    btn_id: 'book__python'
+  },
+  {
+    id: 4,
+    title: 'Background Check for Coders',
+    link: 'https://background.zuri.team',
+    btn_id: 'pitch'
+  },
+  {
+    id: 5,
+    title: 'Design Books',
+    link: 'https://books.zuri.team/design-rules',
+    btn_id: 'book_design'
+  }
+]
